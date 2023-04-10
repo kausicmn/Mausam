76e97c5c56fe949221dd858ae4f8e7f9227a9b60
todeshmukh <104467750+todeshmukh@users.noreply.github.com>
2023-04-09 21:56:56 -0700

    Fetching weather data

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,12 +26,38 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  // final String key ='db03fae9679641cde35e0f9ba462dce9';
+  // final WeatherFactory wf = new WeatherFactory(key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+//
+// final String key ='db03fae9679641cde35e0f9ba462dce9';
+// final WeatherFactory wf = new WeatherFactory(key);
+  late String key;
+  late WeatherFactory wf;
+  Weather? wth;
+
+  Future<void> getCurrentWeather() async {
+    Position loc = await Geolocator.getCurrentPosition();
+    // setState(()  async{
+    wth = await wf.currentWeatherByLocation(loc.latitude, loc.longitude);
+    //
+    // Weather w = await wf.currentWeatherByLocation(loc.latitude, loc.longitude);
+    // wth = w;
+    // Text(" Temperature in celsius: ${w.temperature?.celsius} C");
+  }
+
+  void initState() {
+    super.initState();
+    key = 'db03fae9679641cde35e0f9ba462dce9';
+    wf = new WeatherFactory(key);
+    getCurrentWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,9 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text(" Temperature in celsius: ${wth?.temperature?.celsius} C"),
           ],
         ),
       ),
