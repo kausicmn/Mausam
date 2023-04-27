23619845e5c0b1d74bf7bc35e6305bad19208e71
SantoshLatpate <santoshlatpate12@gmail.com>
2023-04-27 16:09:52 -0700

    Update SecondRoute.dart

--- my_app/lib/SecondRoute.dart
+++ my_app/lib/SecondRoute.dart
@@ -14,50 +14,46 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
 class _MyHomePageState extends State<MyHomePage> {
   String searchValue = '';
-  final List<String> _suggestions = ['Afeganistan', 'Albania', 'Algeria', 'Australia', 'Brazil', 'German', 'Madagascar', 'Mozambique', 'Portugal', 'Zambia'];
+  final List<String> _suggestions = [
+    'Chico',
+    'Sacrmento',
+    'Algeria',
+    'Australia',
+    'Brazil',
+    'German',
+    'Madagascar',
+    'Mozambique',
+    'Portugal',
+    'Zambia'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        title: 'Example',
-        theme: ThemeData(
-            primarySwatch: Colors.orange
-        ),
+    return MaterialApp(
+        title: 'Locations',
+        theme: ThemeData(primarySwatch: Colors.orange),
         home: Scaffold(
             appBar: EasySearchBar(
                 title: const Text('Locations'),
                 onSearch: (value) => setState(() => searchValue = value),
-                suggestions: _suggestions
-            ),
+                suggestions: _suggestions),
             drawer: Drawer(
-                child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      const DrawerHeader(
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                        ),
-                        child: Text('Drawer Header'),
-                      ),
-                      ListTile(
-                          title: const Text('Item 1'),
-                          onTap: () => Navigator.pop(context)
-                      ),
-                      ListTile(
-                          title: const Text('Item 2'),
-                          onTap: () => Navigator.pop(context)
-                      )
-                    ]
-                )
-            ),
-            body: Center(
-                child: Text('Value: $searchValue')
-            )
-        )
-    );
+                child: ListView(padding: EdgeInsets.zero, children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () => Navigator.pop(context)),
+              ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () => Navigator.pop(context))
+            ])),
+            body: Center(child: Text('Value: $searchValue'))));
   }
 }
