9dc08f18bdc9381041214333d61d969724317bbf
SantoshLatpate <santoshlatpate12@gmail.com>
2023-05-09 02:24:14 -0700

    Changes for city select

--- my_app/lib/SecondRoute.dart
+++ my_app/lib/SecondRoute.dart
@@ -4,6 +4,7 @@ import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:my_app/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
+import 'package:country_state_city_picker/country_state_city_picker.dart';
 
 void main() {
   runApp(const SecondPage());
@@ -20,6 +21,9 @@ class _MyHomePageState extends State<SecondPage> {
   String searchValue = '';
   SharedPreferences? prefs;
   List<String> _searchHistory = ['Chico'];
+  String countryValue = '';
+  String stateValue = '';
+  String cityValue = '';
 
   void _addHistory(String element) async {
     if (_searchHistory.length > 0) {
@@ -76,63 +80,100 @@ class _MyHomePageState extends State<SecondPage> {
       title: 'Locations',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            controller: _textController,
-            decoration: const InputDecoration(
-              hintText: 'Enter Location',
-            ),
-            onChanged: (value) {
-              setState(() {
-                _searchValue = value;
-              });
-            },
-            onSubmitted: (value) {
-              setState(() {
-                _addHistory(value);
-              });
-              // Handle the submission here, such as performing a search
-            },
-          ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MyApp()),
-              );
-              // focus the search bar
-            },
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.clear),
-              onPressed: () {
-                _textController.clear();
-                // clear the search bar and results
-              },
-            ),
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: _searchHistory.length, // number of items in the list
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              leading: CircleAvatar(
-                child: Text('${index + 1}'),
+          appBar: AppBar(
+            title: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                hintText: 'Enter Location',
               ),
-              // display index number in a circle avatar
-              title: Text(_searchHistory[index]),
-              //subtitle: Text('Description of Item ${index + 1}'),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                // handle onTap event
-                print('You tapped on Item ${index + 1}');
+              onChanged: (value) {
+                setState(() {
+                  _searchValue = value;
+                });
               },
-            );
-          },
-        ),
-      ),
+              onSubmitted: (value) {
+                setState(() {
+                  _addHistory(value);
+                });
+                // Handle the submission here, such as performing a search
+              },
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                );
+                // focus the search bar
+              },
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.clear),
+                onPressed: () {
+                  _textController.clear();
+                  // clear the search bar and results
+                },
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  height: 600,
+                  child: Column(
+                    children: [
+                      SelectState(
+                        onCountryChanged: (value) {
+                          setState(() {
+                            countryValue = value;
+                          });
+                        },
+                        onStateChanged: (value) {
+                          setState(() {
+                            stateValue = value;
+                          });
+                        },
+                        onCityChanged: (value) {
+                          setState(() {
+                            cityValue = value;
+                          });
+                        },
+                      ),
+                      InkWell(
+                          onTap: () {
+                            print('country selected is $countryValue');
+                            print('country selected is $stateValue');
+                            print('country selected is $cityValue');
+                          },
+                          child: Text(' Check'))
+                    ],
+                  )),
+              Expanded(
+                child: ListView.builder(
+                  itemCount:
+                      _searchHistory.length, // number of items in the list
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      // display index number in a circle avatar
+                      title: Text(_searchHistory[index]),
+                      //subtitle: Text('Description of Item ${index + 1}'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {
+                        // handle onTap event
+                        print('You tapped on Item ${index + 1}');
+                      },
+                    );
+                  },
+                ),
+              )
+            ],
+          )),
     );
   }
 }
